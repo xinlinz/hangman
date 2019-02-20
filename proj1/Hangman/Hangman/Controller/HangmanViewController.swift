e197b4ec8ed99e9128bcf56edff445693699ced4
David Xiong <chromadrive.prime@gmail.com>
2019-02-19 16:31:37 -0800

    Add proj1 skeleton

---
+++ proj1/Hangman/Hangman/Controller/HangmanViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HangmanViewController
+//  Hangman
+//
+//  Created by David [Entei] Xiong on 2/19/19.
+//  Copyright © 2019 iosdecal. All rights reserved.
+//
+
+import UIKit
+
+class HangmanViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ proj1/readme.txt
@@ -0,0 +1,3 @@
+iOS DeCal SP19 // Proj 1
+
+Instructions for this project can be found here: http://iosdecal.com/sp19/proj/proj1/
