3b7f9a469d481cedfbe598fea967259b4660167c
David Xiong <chromadrive.prime@gmail.com>
2019-02-21 16:58:42 -0800

    Add lab4 skeleton

---
+++ lab4/ImagePicker/ImagePicker-Starter/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  ImagePicker-Starter
+//
+//  Created by RJ Pimentel on 2/21/19.
+//  Copyright © 2019 RJ Pimentel. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+
+}
+
---
+++ lab4/readme.txt
@@ -0,0 +1,3 @@
+iOS DeCal SP19 // Lab 4
+
+Instructions for this lab can be found here: http://iosdecal.com/sp19/lab/lab4/
